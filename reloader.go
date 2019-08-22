a6c7d8ed2637337efb9be0a6529a3f6f11810aa9
张酉夫 <zhangyoufu@gmail.com>
2019-08-22 13:40:46 +0800

    go fmt

--- reloader.go
+++ reloader.go
@@ -3,93 +3,99 @@
 package certreloader
 
 import (
-    "bytes"
-    "crypto/tls"
-    "fmt"
-    "io/ioutil"
-    "log"
-    "path/filepath"
-    "sync/atomic"
-    "time"
-    "unsafe"
+	"bytes"
+	"crypto/tls"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+	"sync/atomic"
+	"time"
+	"unsafe"
 )
 
 type Reloader struct {
-    certPath  string
-    keyPath   string
-    certPEM   []byte
-    keyPEM    []byte // BUG: plaintext private key left in memory
-    cert      *tls.Certificate
-    chStop    chan struct{}
+	certPath string
+	keyPath  string
+	certPEM  []byte
+	keyPEM   []byte // BUG: plaintext private key left in memory
+	cert     *tls.Certificate
+	chStop   chan struct{}
 }
 
 // Load and parse certificate and private key in X.509 PEM format. Return a
 // pointer to Reloader if no error happend.
 func New(certPath, keyPath string, interval time.Duration) (*Reloader, error) {
-    var err error
-    if certPath, err = filepath.Abs(certPath); err != nil { return nil, err }
-    if keyPath, err = filepath.Abs(keyPath); err != nil { return nil, err }
-    r := &Reloader {
-        certPath: certPath,
-        keyPath:  keyPath,
-    }
-    if err = r.reload(); err != nil { return nil, err }
-    ticker := time.NewTicker(interval)
-    chStop := make(chan struct{})
-    go func() {
-        <-chStop
-        ticker.Stop()
-    }()
-    r.chStop = chStop
-    go func(ch <-chan time.Time) {
-        for {
-            <-ch
-            if err := r.reload(); err != nil {
-                log.Print(err) // TODO: first error only?
-            }
-        }
-    }(ticker.C)
-    return r, nil
+	var err error
+	if certPath, err = filepath.Abs(certPath); err != nil {
+		return nil, err
+	}
+	if keyPath, err = filepath.Abs(keyPath); err != nil {
+		return nil, err
+	}
+	r := &Reloader{
+		certPath: certPath,
+		keyPath:  keyPath,
+	}
+	if err = r.reload(); err != nil {
+		return nil, err
+	}
+	ticker := time.NewTicker(interval)
+	chStop := make(chan struct{})
+	go func() {
+		<-chStop
+		ticker.Stop()
+	}()
+	r.chStop = chStop
+	go func(ch <-chan time.Time) {
+		for {
+			<-ch
+			if err := r.reload(); err != nil {
+				log.Print(err) // TODO: first error only?
+			}
+		}
+	}(ticker.C)
+	return r, nil
 }
 
 // Stop futher reloading. A stopped reloader cannot be started again. Loaded
 // certificate is still available. Call this method if you don't want resource
 // leak.
 func (r *Reloader) Stop() {
-    select {
-    case <-r.chStop:
-    default:
-        close(r.chStop)
-    }
+	select {
+	case <-r.chStop:
+	default:
+		close(r.chStop)
+	}
 }
 
 func (r *Reloader) reload() error {
-    var (
-        err     error
-        certPEM []byte
-        keyPEM  []byte
-        cert    tls.Certificate
-    )
-    if certPEM, err = ioutil.ReadFile(r.certPath); err != nil {
-        return fmt.Errorf("unable to read certificate: %v", err)
-    }
-    if keyPEM, err = ioutil.ReadFile(r.keyPath); err != nil {
-        return fmt.Errorf("unable to read private key: %v", err)
-    }
-    if bytes.Equal(certPEM, r.certPEM) && bytes.Equal(keyPEM, r.keyPEM) {
-        return nil
-    }
-    if cert, err = tls.X509KeyPair(certPEM, keyPEM); err != nil {
-        return err
-    }
-    r.certPEM = certPEM
-    r.keyPEM = keyPEM
-    atomic.StorePointer((*unsafe.Pointer)(unsafe.Pointer(&r.cert)), unsafe.Pointer(&cert))
-    return nil
+	var (
+		err     error
+		certPEM []byte
+		keyPEM  []byte
+		cert    tls.Certificate
+	)
+	if certPEM, err = ioutil.ReadFile(r.certPath); err != nil {
+		return fmt.Errorf("unable to read certificate: %v", err)
+	}
+	if keyPEM, err = ioutil.ReadFile(r.keyPath); err != nil {
+		return fmt.Errorf("unable to read private key: %v", err)
+	}
+	if bytes.Equal(certPEM, r.certPEM) && bytes.Equal(keyPEM, r.keyPEM) {
+		return nil
+	}
+	if cert, err = tls.X509KeyPair(certPEM, keyPEM); err != nil {
+		return err
+	}
+	r.certPEM = certPEM
+	r.keyPEM = keyPEM
+	atomic.StorePointer((*unsafe.Pointer)(unsafe.Pointer(&r.cert)), unsafe.Pointer(&cert))
+	return nil
 }
 
 // Get current *tls.Certificate.
 func (r *Reloader) Get() *tls.Certificate {
-    return (*tls.Certificate)(atomic.LoadPointer(
-        (*unsafe.Pointer)(unsafe.Pointer(&r.cert))))
+	return (*tls.Certificate)(atomic.LoadPointer(
+		(*unsafe.Pointer)(unsafe.Pointer(&r.cert))))
 }
--- reloader_test.go
+++ reloader_test.go
@@ -1,35 +1,35 @@
 package certreloader_test
 
 import (
-    "crypto/tls"
-    "log"
-    "net/http"
-    "time"
+	"crypto/tls"
+	"log"
+	"net/http"
+	"time"
 
-    "github.com/zhangyoufu/certreloader"
+	"github.com/zhangyoufu/certreloader"
 )
 
 func Example() {
-    const (
-        listenAddr = "localhost:8443"
-        certPath = "path/to/fullchain.pem"
-        keyPath = "path/to/privkey.pem"
-        reloadInterval = 5 * time.Minute
-    )
+	const (
+		listenAddr     = "localhost:8443"
+		certPath       = "path/to/fullchain.pem"
+		keyPath        = "path/to/privkey.pem"
+		reloadInterval = 5 * time.Minute
+	)
 
-    reloader, err := certreloader.New(certPath, keyPath, reloadInterval)
-    if err != nil {
-        // unable to load certificate / private key
-        log.Fatal(err)
-    }
-    server := http.Server {
-        Addr: listenAddr,
-        TLSConfig: &tls.Config {
-            GetCertificate: func(*tls.ClientHelloInfo) (*tls.Certificate, error) {
-                return reloader.Get(), nil
-            },
-        },
-    }
-    err = server.ListenAndServeTLS("", "")
-    log.Fatal(err)
+	reloader, err := certreloader.New(certPath, keyPath, reloadInterval)
+	if err != nil {
+		// unable to load certificate / private key
+		log.Fatal(err)
+	}
+	server := http.Server{
+		Addr: listenAddr,
+		TLSConfig: &tls.Config{
+			GetCertificate: func(*tls.ClientHelloInfo) (*tls.Certificate, error) {
+				return reloader.Get(), nil
+			},
+		},
+	}
+	err = server.ListenAndServeTLS("", "")
+	log.Fatal(err)
 }
